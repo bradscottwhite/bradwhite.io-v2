a13b4b9c5e5215cfc4230720108c9be2027c445c
Brad White <bradscottwhite@gmail.com>
2023-07-08 16:21:18 -0400

    fixing another type error..

--- components/SectionWrapper.tsx
+++ components/SectionWrapper.tsx
@@ -1,8 +1,8 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 //import { motion } from 'framer-motion';
 
 interface wrapperType {
-  children: FC;
+  children: ReactNode;
   idName: string;
   index: number;
 }
@@ -18,20 +18,6 @@ const SectionWrapper: FC<wrapperType> = ({
       id={idName}
       style={
         index % 2 == 0 ? {
-          /*backgroundColor: 'rgb(var(--dark-3))', // darkest violet
-          //change '--dark-2' 3rd line to just dark violet and not darkest violet (dark-4)??!!!!
-          //5th line???!!!
-          backgroundImage: `
-            radial-gradient(at 34% 96%, rgb(var(--accent-2)) 0, transparent 67%),
-            radial-gradient(at 4% 72%, rgb(var(--dark-1)) 0, transparent 55%),
-            radial-gradient(at 87% 44%, rgb(var(--dark-2)) 0, transparent 24%),
-            radial-gradient(at 40% 1%, rgb(var(--dark-1)) 0, transparent 38%),
-            radial-gradient(at 77% 5%, rgb(var(--accent-3)) 0, transparent 77%),
-            radial-gradient(at 61% 18%, rgb(var(--accent-1)) 0, transparent 54%)
-          `*/
-          /*backgroundColor: 'rgb(77, 124, 15)',
-          backgroundImage: 'radial-gradient(at 34% 96%, rgb(94, 234, 212) 0, transparent 67%), radial-gradient(at 4% 72%, rgb(185, 28, 28) 0, transparent 55%), radial-gradient(at 87% 44%, rgb(202, 138, 4) 0, transparent 24%), radial-gradient(at 40% 1%, rgb(154, 52, 18) 0, transparent 38%), radial-gradient(at 77% 5%, rgb(239, 68, 68) 0, transparent 77%), radial-gradient(at 61% 18%, rgb(254, 240, 138) 0, transparent 54%)'*/
-
           backgroundColor: '#C8CED0',
           backgroundImage: `
             radial-gradient(at 19% 53%, #59C5A6 0px, transparent 50%),
