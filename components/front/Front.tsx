beeb2fc398713c1c378e337597a1e026a99b0d6a
Brad White <bradscottwhite@gmail.com>
2023-07-28 07:01:52 -0400

    ..

--- components/front/Front.tsx
+++ components/front/Front.tsx
@@ -17,11 +17,17 @@ import Link from 'next/link';
 const Front = () => {
   return (
     <div className='py-[28vh] pl-[15vw]'>
-      <h1 className='bg-gradient-to-r from-sec-accent-1 via-sec-accent-3 to-sec-accent-2 bg-clip-text text-transparent text-5xl py-2 opacity-75 font-crenzo' style={{ filter: 'blur(1px)' }}>Brad White</h1>
+      {/*<h1 className='bg-gradient-to-r from-sec-accent-1 via-sec-accent-3 to-sec-accent-2 bg-clip-text text-transparent text-5xl py-2 opacity-75 font-crenzo' style={{ filter: 'blur(1px)' }}>Brad White</h1>
       <h2 className='bg-gradient-to-r from-pri-accent-1 via-pri-accent-3 to-pri-accent-2 bg-clip-text text-transparent text-2xl text-slate-100 pb-2 font-baunk' style={{ filter: 'blur(1px)' }}>Full-stack Software Engineer</h2>
       <p className='bg-gradient-to-r from-pri-light-1 via-pri-light-3 to-pri-light-2 bg-clip-text text-transparent text-lg text-slate-100 pb-2'>
         Building functional and elequent apps on the wild web
+      </p>*/}
+      <h1 className='text-[rgba(var(--pri-dark-1),0.75)] text-5xl py-2 opacity-75 font-crenzo' style={{ filter: 'blur(1px)' }}>Brad White</h1>
+      <h2 className='text-[rgba(var(--sec-dark-1),0.75)] text-2xl text-slate-100 pb-2 font-baunk' style={{ filter: 'blur(1px)' }}>Full-stack Software Engineer</h2>
+      <p className='text-sec-light-1 text-2xl text-slate-100 py-2'>
+        Building <span className='bg-gradient-to-r from-sec-dark-1 via-sec-dark-2 to-sec-dark-3 bg-clip-text text-transparent after:bg-gradient-to-r after:from-sec-dark-1 after:via-sec-dark-2 after:to-sec-dark-3 after:bg-clip-text after:text-transparent after:bg-[length:100%_5px] after:bg-no-repeat after:bg-bottom'>functional</span> and <span className='bg-gradient-to-r from-sec-dark-1 via-sec-dark-2 to-sec-dark-3 bg-clip-text text-transparent underline'>elequent</span> apps on the wild web
       </p>
+
       <div className='md:w-[45vw] w-full pt-4 pb-8 grid grid-cols-6 gap-4'>
         <TSIcon size='3em' />
         <ReactIcon size='3em' />
@@ -37,24 +43,6 @@ const Front = () => {
       </div>
     </div>
   );
-      /* Scroll Motion */
-    /*<div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center z-10'>
-        <Link href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-slate-400 flex justify-center items-start p-2'>
-            <motion.div
-              animate={{y: [0, 24, 0]}}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: 'loop'
-              }}
-              className='w-3 h-3 rounded-full bg-slate-400 mb-1'
-            ></motion.div>
-          </div>
-        </Link>
-      </div>*/
-    //</div>
-    //);
 };
 
 export default Front;
