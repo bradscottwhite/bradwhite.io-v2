196fb889e67f75ff1fa728f598aec77c4bf46014
Brad White <bradscottwhite@gmail.com>
2023-08-12 23:48:03 -0400

    updated projects

--- components/projects/projects.ts
+++ components/projects/projects.ts
@@ -19,25 +19,25 @@ export interface projectType {
 
 export const projects: projectType[] = [
   {
-    title: 'Project 1',
-    desc: 'Full dynamic SSR application using Next, React, Tailwind, and TypeScript.',
+    title: 'Scalable Blog App',
+    desc: 'Scalable multi-user authenticated app components that can be quickly and easily implemented into other projects',
     img: '/project_pic_1.png',
-    url: '#project1', //Build a project page????
-    github: 'https://www.github.com/bradscottwhite',
-    tech: [ TSIcon, ReactIcon, TWIcon ]
+    url: 'https://bp-app.netlify.app/', //Build a project page????
+    github: 'https://www.github.com/bradscottwhite/bp',
+    tech: [ TSIcon, ReactIcon, TWIcon, GraphQLIcon ]
   }, {
-    title: 'Project 2',
-    desc: 'Full dynamic SSR application using Next, React, Tailwind, TypeScript, and Node.',
+    title: 'Minimalist Social Media App',
+    desc: 'A scalable and affordable social media application',
+    img: '/project_pic_2.png',
+    url: 'https://master.d3e9ksev6svovb.amplifyapp.com/',
+    github: 'https://github.com/bradscottwhite/firebirdie',
+    tech: [ ReactIcon, TWIcon, AWSIcon ]
+  }, {
+    title: 'Productivity Planner App',
+    desc: 'Multi-user authenticated planner app similar to Trello',
     img: '/project_pic_3.png',
-    url: '#project2',
-    github: 'https://www.github.com/bradscottwhite',
-    tech: [ TSIcon, ReactIcon, TWIcon, NodeIcon ]
-  }, {
-    title: 'Project 3',
-    desc: 'Full dynamic SSR application using TypeScript, GraphQL, AWS, and Node.',
-    img: '/project_pic_1.png',
-    url: '#project3',
-    github: 'https://www.github.com/bradscottwhite',
-    tech: [ TSIcon, GraphQLIcon, AWSIcon, NodeIcon ]
-  },
+    url: 'https://bradscottwhite.github.io/min-planner/',
+    github: 'https://github.com/bradscottwhite/min-planner',
+    tech: [ ReactIcon, TWIcon, AWSIcon ]
+  }
 ];
